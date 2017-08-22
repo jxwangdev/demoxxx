09b7684ab3fd58d2fc3d5a8cdb9e2c8a113706d5
Jessica409 <jxwangsh@cn.ibm.com>
2017-08-22 05:58:08 -0700

    first commit

---
+++ src/main/java/application/rest/v1/Example.java
@@ -0,0 +1,35 @@
+package application.rest.v1;
+
+import java.util.List;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import java.util.ArrayList;
+
+
+// @Path("v1/example")
+// public class Example {
+//
+//
+//     @GET
+//     @Produces(MediaType.TEXT_PLAIN)
+//     public Response example() {
+//         List<String> list = new ArrayList<>();
+//         //return a simple list of strings
+//         list.add("Some data");
+//         return Response.ok(list.toString()).build();
+//     }
+//
+//
+// }
+
+@Path("hello")
+public class Example {
+ @GET
+ public String greet() throws Exception {
+   return "Hello from " + java.net.InetAddress.getLocalHost().getHostName();
+ }
+}
